eb6800d4db3275cb0be6fe34a3aae49bbbc7344c
Cohnante <gian-5634@hotmail.com>
2023-09-04 19:06:06 -0500

    Correciones.DMV

--- jardineria-mongodb.js
+++ jardineria-mongodb.js
@@ -509,4 +509,4 @@ db.pago.find({}, { forma_pago: 1, _id: 0 });
 // 15. Devuelve un listado con todos los productos que pertenecen a la gama Ornamentales y que tienen más de 100 unidades en stock. El listado deberá estar ordenado por su precio de venta, mostrando en primer lugar los de mayor precio.
 db.producto.find({ gama: 'Ornamentales', cantidad_en_stock: { $gt: 100 } }).sort({ precio_venta: -1 });
 // 16. Devuelve un listado con todos los clientes que sean de la ciudad de Madrid y cuyo representante de ventas tenga el código de empleado 11 o 30
-db.cliente.find({ ciudad: 'Madrid', codigo_empleado_rep_ventas: { $in: [11, 30] } });
+db.cliente.find({ ciudad: 'Madrid', codigo_empleado_rep_ventas: { $in: [{$or:[{11},{30}]}] } });
